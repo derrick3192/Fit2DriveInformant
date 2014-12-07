4340a07d4fca42ae24395de1916231b0b43821cf
Derrick Futschik <derrickfutschik@hotmail.com>
2014-12-07 10:37:29 +0100

    Test email failure

---
+++ Fit2DriveInformant/src/test/java/fit2drive/TestTest.java
@@ -0,0 +1,14 @@
+package fit2drive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+	
+	@Test
+	public void testfail() {
+		fail("Test email the perpatraitor which failed a test");
+	}
+
+}
